2ce4930583895b1f8f3abae50568fec55b059c9c
a1374077658 <1374077658@qq.com>
2022-10-20 21:48:36 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,27 @@
+var config = {
+    texts: [
+        "送给",     
+        "最美丽的小婷",  
+        "今天是你的生日",
+        "走过漫漫的岁月",
+        "细数片片的回忆",
+        "请记得我",
+        "就像我时时想起你一般",
+        "新的一岁里",
+        "愿你平安快乐",
+        "美梦成真",
+    ],
+    imgs: {
+        "心爱的小可爱": "./imgs/xiaokeai.png",
+    },
+    desc: {
+        turn_on: "开始",
+        play: "音乐",
+        bannar_coming: "颜色",
+        balloons_flying: "好像少点东西",
+        cake_fadein: "蛋糕？",
+        light_candle: "蜡烛？",
+        wish_message: "生日快乐",
+        story: "A MESSAGE FOR YOU",
+    }
+};
